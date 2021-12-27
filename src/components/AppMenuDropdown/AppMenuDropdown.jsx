cbce1ca69ca847d39cb6db310dbaf67b8dd7971a
Ross <rostyslav.tykhovski@gmail.com>
2021-12-27 14:02:07 +0200

    fix modal style

--- src/components/AppMenuDropdown/AppMenuDropdown.jsx
+++ src/components/AppMenuDropdown/AppMenuDropdown.jsx
@@ -1,85 +1,127 @@
 import './AppMenuDropdown.css';
 import AppMenuItem from '../AppMenuItem/AppMenuItem';
-import TranslateLogo from '../../assets/google_translate.png';
-import DocsLogo from '../../assets/google_docs.png';
-import YouTubeLogo from '../../assets/youtube_logo.png';
-import SheetsLogo from '../../assets/google_sheets.png';
-import GmailLogo from '../../assets/gmail_logo.png';
-import KeepLogo from '../../assets/google_keep.png';
-import DriveLogo from '../../assets/google_drive.png';
-import MapsLogo from '../../assets/maps_logo.png';
-import PhotoLogo from '../../assets/google_photo_logo.png';
-import CalendarLogo from '../../assets/google_calendar.png';
-import MeetsLogo from '../../assets/meets_logo.png';
+import Firebase from '../../assets/firebase.png';
+import Documents from '../../assets/documents.png';
+import YouTube from '../../assets/youtube.png';
+import Sheets from '../../assets/sheets.png';
+import Gmail from '../../assets/gmail.png';
+import Keep from '../../assets/keep.png';
+import Drive from '../../assets/drive.png';
+import Maps from '../../assets/maps.png';
+import Photo from '../../assets/photo.png';
+import Calendar from '../../assets/calendar.png';
+import Meets from '../../assets/meets.png';
+import Duo from '../../assets/duo.png';
+import Chats from '../../assets/chats.png';
+import Chrome from '../../assets/chrome.png';
+import Slides from '../../assets/slides.png';
+import News from '../../assets/news.png';
+import TensorFlow from '../../assets/tensor-flow.png';
 
 import {Link} from 'react-router-dom';
 
 const AppMenuDropdown = () => {
   return (
     <div className="dropdown">
-      <Link to='/translate'>
+      <Link to='/firebase'>
         <AppMenuItem
-          image={TranslateLogo}
-          alt='google-translate'
-          title='Translate' />
+          image={Firebase}
+          alt='google-firebase'
+          title='Firebase' />
+      </Link>
+      <Link to='/news'>
+        <AppMenuItem
+          image={News}
+          alt='google-news'
+          title='News' />
+      </Link>
+      <Link to='/tensorflow'>
+        <AppMenuItem
+          image={TensorFlow}
+          alt='google-tensorglow'
+          title='Tensor Flow' />
+      </Link>
+      <Link to='/duo'>
+        <AppMenuItem
+          image={Duo}
+          alt='google-duo'
+          title='Duo' />
+      </Link>
+      <Link to='/chats'>
+        <AppMenuItem
+          image={Chats}
+          alt='google-chats'
+          title='Chats' />
+      </Link>
+      <Link to='/chrome'>
+        <AppMenuItem
+          image={Chrome}
+          alt='google-chrome'
+          title='Chrome' />
+      </Link>
+      <Link to='/slides'>
+        <AppMenuItem
+          image={Slides}
+          alt='google-slides'
+          title='Slides' />
       </Link>
       <Link to='/documents'>
         <AppMenuItem
-          image={DocsLogo}
+          image={Documents}
           alt='google-documents'
           title='Documents' />
       </Link>
       <Link to='/youtube'>
         <AppMenuItem
-          image={YouTubeLogo}
+          image={YouTube}
           alt='You-tube'
           title='You Tube' />
       </Link>
       <Link to='/sheets'>
         <AppMenuItem
-          image={SheetsLogo}
+          image={Sheets}
           alt='google-sheets'
           title='Sheets' />
       </Link>
       <Link to='/gmail'>
         <AppMenuItem
-          image={GmailLogo}
+          image={Gmail}
           alt='google-gmail'
           title='Gmail' />
       </Link>
       <Link to='/keep'>
         <AppMenuItem
-          image={KeepLogo}
+          image={Keep}
           alt='google-keep'
           title='Keep' />
       </Link>
       <Link to='/drive'>
         <AppMenuItem
-          image={DriveLogo}
+          image={Drive}
           alt='google-drive'
           title='Drive' />
       </Link>
       <Link to='/maps'>
         <AppMenuItem
-          image={MapsLogo}
+          image={Maps}
           alt='google-maps'
           title='Maps' />
       </Link>
       <Link to='/photo'>
         <AppMenuItem
-          image={PhotoLogo}
+          image={Photo}
           alt='google-photo'
           title='Photo' />
       </Link>
       <Link to='/calendar'>
         <AppMenuItem
-          image={CalendarLogo}
+          image={Calendar}
           alt='google-calendar'
           title='Calendar' />
       </Link>
       <Link to='/meets'>
         <AppMenuItem
-          image={MeetsLogo}
+          image={Meets}
           alt='google-meets'
           title='Meets' />
       </Link>
