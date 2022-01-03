badf92d7bd514dd7446e05bf28e25a6fab996fd7
Ross <rostyslav.tykhovski@gmail.com>
2022-01-03 14:09:46 +0200

    fix dnd, fetch data from firebase

--- src/components/AppMenuDropdown/AppMenuDropdown.jsx
+++ src/components/AppMenuDropdown/AppMenuDropdown.jsx
@@ -3,137 +3,30 @@ import AppMenuItem from '../AppMenuItem/AppMenuItem';
 
 import {Link} from 'react-router-dom';
 
-import {useDispatch} from 'react-redux';
-import {appDropdownAction} from '../../redux/appDropdown/appDropdownSlice';
-
 import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd';
-import {useState} from 'react';
-
-
-const googleApps = [
-  {
-    id: 'calendar',
-    title: 'Calendar',
-    alt: 'google-calendar',
-    image: '/images/calendar.png',
-    link: '/calendar',
-  },
-  {
-    id: 'chats',
-    title: 'Chats',
-    alt: 'google-chats',
-    image: '/images/chats.png',
-    link: '/chats',
-  },
-  {
-    id: 'chrome',
-    title: 'Chrome',
-    alt: 'google-chrome',
-    image: '/images/chrome.png',
-    link: '/chrome',
-  },
-  {
-    id: 'documents',
-    title: 'Documents',
-    alt: 'google-documents',
-    image: '/images/documents.png',
-    link: '/documents',
-  },
-  {
-    id: 'drive',
-    title: 'Drive',
-    alt: 'google-drive',
-    image: '/images/drive.png',
-    link: '/drive',
-  },
-  {
-    id: 'duo',
-    title: 'Duo',
-    alt: 'google-duo',
-    image: '/images/duo.png',
-    link: '/duo',
-  },
-  {
-    id: 'gmail',
-    title: 'Gmail',
-    alt: 'google-gmail',
-    image: '/images/gmail.png',
-    link: '/gmail',
-  },
-  {
-    id: 'keep',
-    title: 'Keep',
-    alt: 'google-keep',
-    image: '/images/keep.png',
-    link: '/keep',
-  },
-  {
-    id: 'maps',
-    title: 'Maps',
-    alt: 'google-maps',
-    image: '/images/maps.png',
-    link: '/maps',
-  },
-  {
-    id: 'meets',
-    title: 'Meets',
-    alt: 'google-meets',
-    image: '/images/meets.png',
-    link: '/meets',
-  },
-  {
-    id: 'news',
-    title: 'News',
-    alt: 'google-news',
-    image: '/images/news.png',
-    link: '/news',
-  },
-  {
-    id: 'photo',
-    title: 'Photo',
-    alt: 'google-photo',
-    image: '/images/photo.png',
-    link: '/photo',
-  },
-  {
-    id: 'sheets',
-    title: 'Sheets',
-    alt: 'google-sheets',
-    image: '/images/sheets.png',
-    link: '/sheets',
-  },
-  {
-    id: 'slides',
-    title: 'Slides',
-    alt: 'google-slides',
-    image: '/images/slides.png',
-    link: '/slides',
-  },
-  {
-    id: 'tensor-flow',
-    title: 'Tensor Flow',
-    alt: 'google-tensor-flow',
-    image: '/images/tensor-flow.png',
-    link: '/tensor-flow',
-  },
-  {
-    id: 'youtube',
-    title: 'Youtube',
-    alt: 'google-youtube',
-    image: '/images/youtube.png',
-    link: '/youtube',
-  },
-  {
-    id: 'firebase',
-    title: 'Firebase',
-    alt: 'google-firebase',
-    image: '/images/firebase.png',
-    link: '/firebase',
-  },
-];
+import {useState, useEffect} from 'react';
 
 const AppMenuDropdown = () => {
-  const [apps, setApps] = useState(googleApps);
+  const [apps, setApps] = useState([]);
+  useEffect(()=>{
+    const fetchData = async () => {
+      const response = await fetch('https://clone-a2884-default-rtdb.europe-west1.firebasedatabase.app/apps.json');
+      const responseData = await response.json();
+      const data = [];
+      // eslint-disable-next-line guard-for-in
+      for (const key in responseData) {
+        data.push({
+          id: key,
+          title: responseData[key].title,
+          alt: responseData[key].alt,
+          link: responseData[key].link,
+          image: responseData[key].image,
+        });
+      }
+      setApps(data);
+    };
+    fetchData();
+  }, []);
 
   const handleOnDragEnd = (result) => {
     if (!result.destination) return;
@@ -144,45 +37,42 @@ const AppMenuDropdown = () => {
     setApps(items);
   };
 
-  const dispatch = useDispatch();
-  const closeModal = () => {
-    dispatch(appDropdownAction.closeModal());
-  };
   return (
-    <DragDropContext onDragEnd={handleOnDragEnd}>
-      <Droppable droppableId="characters">
-        {(provided) => (
-          <div
-            className="dropdown"
-            onClick={closeModal}
-            {...provided.droppableProps}
-            ref={provided.innerRef}
-          >
-            {googleApps.map(({id, title, alt, image, link}, index)=> {
-              return (
-                <Draggable key={id} draggableId={id} index={index}>
-                  {(provided) => (
-                    <Link
-                      to={link}
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                    >
-                      <AppMenuItem
-                        image={image}
-                        alt={alt}
-                        title={title}
-                      />
-                    </Link>
-                  )}
-                </Draggable>
-              );
-            })}
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
-    </DragDropContext>
+    <div className="dropdown">
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Droppable droppableId="droppable">
+          {(provided) => (
+            <div
+              className="wrapper"
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {apps.map(({id, title, alt, image, link}, index)=> {
+                return (
+                  <Draggable key={id} draggableId={id} index={index}>
+                    {(provided) => (
+                      <Link
+                        to={link}
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
+                        <AppMenuItem
+                          image={image}
+                          alt={alt}
+                          title={title}
+                        />
+                      </Link>
+                    )}
+                  </Draggable>
+                );
+              })}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
   );
 };
 
